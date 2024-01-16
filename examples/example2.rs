2a0c5e9b546e029b8243390506bbf8fd66943411
Yun Seob Lee <fhzotxldj@gmail.com>
2024-01-16 20:23:05 +0900

    feat: add new example for spinner functionality

---
+++ examples/example2.rs
@@ -0,0 +1,20 @@
+use cli_prompts_rs::CliPrompt;
+use std::{thread, time};
+
+fn main() {
+    let mut cli_prompt = CliPrompt::new();
+
+    cli_prompt.intro("spinner example").unwrap();
+
+    cli_prompt
+        .print_note("This example shows how to use spinner feature")
+        .unwrap();
+    let task = || {
+        thread::sleep(time::Duration::from_millis(5000));
+    };
+    cli_prompt
+        .call_spinner("working", "Done!", 10000, task)
+        .unwrap();
+
+    cli_prompt.outro("Good Bye").unwrap();
+}
